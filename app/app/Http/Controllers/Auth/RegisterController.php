cf37204c5ad006a02fe4106b0a906ad90e38e848
ryo-ebata <r.ebata@hajimari.inc>
2022-04-17 15:36:08 +0900

    新規登録でscreen_nameの入力を追加しました

--- app/app/Http/Controllers/Auth/RegisterController.php
+++ app/app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'screen_name' => $data['screen_name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
--- app/resources/views/auth/register.blade.php
+++ app/resources/views/auth/register.blade.php
@@ -26,6 +26,20 @@
                         </div>
 
                         <div class="row mb-3">
+                            <label for="screen_name" class="col-md-4 col-form-label text-md-end">{{ __('Screen Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="screen_name" type="text" class="form-control @error('screen_name') is-invalid @enderror" name="screen_name" value="{{ old('screen_name') }}" required autocomplete="screen_name" autofocus>
+
+                                @error('screen_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
